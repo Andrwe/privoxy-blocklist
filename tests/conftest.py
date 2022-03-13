1bce4a10bb9195d2af9315ebebce41a7823a570b
Andrwe Lord Weber <github@andrwe.org>
2022-03-13 12:44:53 +0100

    implement #25 & several improvements
    * implement option `-c` to specify script config file
    * implement OS specific defaults for script config file
    * increase version to 0.4
    * move code to dedicated functions to reduce requirements for different calls
    * add info output function
    * add Dockerfile to run test suite on Ubuntu
    * improve tests/run.sh to allow running on different OS container
    * add test for dependency check
    * add test for custom script config path
    
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,11 @@ import pytest
 @pytest.fixture
 def privoxy_blocklist():
     """return the path to privoxy-blocklist.sh"""
-    for known_path in ["/privoxy-blocklist.sh", "./privoxy-blocklist.sh"]:
+    for known_path in [
+        "./privoxy-blocklist.sh",
+        "/privoxy-blocklist.sh",
+        "/app/privoxy-blocklist.sh",
+    ]:
         path = Path(known_path)
         if path.exists() and path.is_file() and os.access(path, os.X_OK):
             return str(path.absolute())
