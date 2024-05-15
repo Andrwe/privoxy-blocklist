3bddb361b89ce6efaa949516ff940079bfd1106e
Andrwe@Gitlab <alw+gitlab@andrwe.org>
2024-05-16 00:56:36 +0200

    add os 'all' to check for all OSes
    Signed-off-by: Andrwe@Gitlab <alw+gitlab@andrwe.org>

--- tests/run.sh
+++ tests/run.sh
@@ -32,33 +32,53 @@ done
 
 shift "${skip}"
 
-img_tag="privoxy-blocklist-test:${os}"
-dockerfile="${SCRIPT_DIR}/Dockerfile_${os}"
-pytest_cache="pytest_cache_${os}"
+oses="${os}"
+fails=""
 
-if ! [ -f "${dockerfile}" ]; then
-    echo "given OS '${os}' is not supported ('${dockerfile}' missing)"
-    exit 1
+if [ "${os}" = "all" ]; then
+    oses=""
+    for config in "${SCRIPT_DIR}/Dockerfile_"*; do
+        oses="${oses} ${config##*/Dockerfile_}"
+    done
 fi
 
-if [ "${rebuild}" = "true" ] || ! docker image ls -q "${img_tag}" | grep -vq '^$' || [ "$(($(date +%s) - $(date -d "$(docker image ls "${img_tag}" --format '{{print .CreatedAt}}' | cut -d' ' -f1,2)" +%s)))" -gt 14400 ]; then
-    echo "building docker image for ${os}"
-    cd "${GIT_DIR}"
-    if docker image ls --format '{{ .Repository }}:{{ .Tag }}' | grep -q "${img_tag}"; then
-        docker image rm "${img_tag}" > /dev/null || true
+for os in ${oses}; do
+    img_tag="privoxy-blocklist-test:${os}"
+    dockerfile="${SCRIPT_DIR}/Dockerfile_${os}"
+    pytest_cache="pytest_cache_${os}"
+
+    if ! [ -f "${dockerfile}" ]; then
+        echo "given OS '${os}' is not supported ('${dockerfile}' missing)"
+        exit 1
     fi
-    if docker volume ls --format '{{ .Name }}' | grep -q "${pytest_cache}"; then
-        docker volume rm "${pytest_cache}" > /dev/null || true
+
+    if [ "${rebuild}" = "true" ] || ! docker image ls -q "${img_tag}" | grep -vq '^$' || [ "$(($(date +%s) - $(date -d "$(docker image ls "${img_tag}" --format '{{print .CreatedAt}}' | cut -d' ' -f1,2)" +%s)))" -gt 14400 ]; then
+        echo "building docker image for ${os}"
+        cd "${GIT_DIR}"
+        if docker image ls --format '{{ .Repository }}:{{ .Tag }}' | grep -q "${img_tag}"; then
+            docker image rm "${img_tag}" > /dev/null || true
+        fi
+        if docker volume ls --format '{{ .Name }}' | grep -q "${pytest_cache}"; then
+            docker volume rm "${pytest_cache}" > /dev/null || true
+        fi
+        docker build -q -t "${img_tag}" -f "${dockerfile}" .
+        cd -
+        echo
     fi
-    docker build -q -t "${img_tag}" -f "${dockerfile}" .
-    cd -
+
+    if [ "${interactive}" -eq 0 ]; then
+        echo "running tests on ${os}"
+        if ! docker run --rm -w /app -v "${GIT_DIR}:/app" -v "${pytest_cache}:/pytest_cache" "${img_tag}" "${@:-./tests}"; then
+            fails="${fails} ${os}"
+        fi
+    else
+        echo "interactive mode on ${os}"
+        docker run -ti --rm -w /app -v "${GIT_DIR}:/app" -v "${pytest_cache}:/pytest_cache" --entrypoint /bin/bash "${img_tag}"
+    fi
+done
+
+if [ -n "${fails}" ]; then
     echo
-fi
-
-if [ "${interactive}" -eq 0 ]; then
-    echo "running tests on ${os}"
-    docker run --rm -w /app -v "${GIT_DIR}:/app" -v "${pytest_cache}:/pytest_cache" "${img_tag}" "${@:-./tests}"
-else
-    echo "interactive mode on ${os}"
-    docker run -ti --rm -w /app -v "${GIT_DIR}:/app" -v "${pytest_cache}:/pytest_cache" --entrypoint /bin/bash "${img_tag}"
+    echo "Failed: ${fails}"
+    exit 1
 fi
